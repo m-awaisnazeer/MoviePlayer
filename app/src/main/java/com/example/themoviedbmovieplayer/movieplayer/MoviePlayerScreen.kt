be3b5c32a46af5950a065ce2756fb984b1d56508
awais ahmad <awais.ahmad@jeeny.me>
2025-05-25 13:17:14 +0500

    core:ui module created

--- app/src/main/java/com/example/themoviedbmovieplayer/movieplayer/MoviePlayerScreen.kt
+++ app/src/main/java/com/example/themoviedbmovieplayer/movieplayer/MoviePlayerScreen.kt
@@ -5,14 +5,9 @@ import android.app.Activity
 import android.content.pm.ActivityInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.media3.common.MediaItem
-import androidx.media3.common.MimeTypes
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
+import com.example.ui.VideoPlayer
 
 @Composable
 fun MoviePlayerScreen(modifier: Modifier = Modifier) {
